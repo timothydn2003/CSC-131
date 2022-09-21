5bfc4b26ba2325111d951a2d15a46323b164c227
Timothy Doan <87452000+timothydn2003@users.noreply.github.com>
2022-09-20 21:37:06 -0700

    add new person feature

--- client/src/Pages/NewPerson.js
+++ client/src/Pages/NewPerson.js
@@ -38,15 +38,15 @@ const NewPerson = () => {
                 <label>Drivers License Number:</label><br></br>
                 <input onChange={(e) => setDl(e.target.value)}/>
                 <label>Date of Birth:</label><br></br>
-                <input onChange={(e) => setDob(e.target.value)}/>
+                <input onChange={(e) => setDob(e.target.value)} type = 'date'/>
                 <label>SSN:</label><br></br>
                 <input onChange={(e) => setSsn(e.target.value)}/>
                 <label>Image:</label><br></br>
-                <input onChange={(e) => setImage(e.target.value)}/>
+                <input onChange={(e) => setImage(e.target.value)} type = 'url'/>
                 <label>Passport Number:</label><br></br>
                 <input onChange={(e) => setPassport(e.target.value)}/>
                 <label>Passport Expiration:</label><br></br>
-                <input onChange={(e) => setPassportExpir(e.target.value)}/>
+                <input onChange={(e) => setPassportExpir(e.target.value)} type = 'date'/>
                 <button type='submit' onClick={send}>Submit</button>
             </form>
         </div>
