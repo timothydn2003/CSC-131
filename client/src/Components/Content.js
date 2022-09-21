2ffbb83a1d3306dd560cab7439345af57525ef33
Timothy Doan <87452000+timothydn2003@users.noreply.github.com>
2022-09-20 21:40:56 -0700

    Update Content.js

--- client/src/Components/Content.js
+++ client/src/Components/Content.js
@@ -8,10 +8,10 @@ const Content = (props) => {
         <Row>
             <Col md = "4" xs = "12">
                 <h1>DMV</h1>
+                <img className='image' src= {props.person.image}></img>
                 <h5>{props.person.name}</h5>
                 <h5>{props.person.dl}</h5>
                 <h5>{props.person.dob}</h5>
-                <img className='image' src= {props.person.image}></img>
             </Col>
             <Col md = "4" xs = "12"> 
                 <h1>SS</h1>
@@ -20,12 +20,12 @@ const Content = (props) => {
             </Col>
             <Col md = "4" xs = "12"> 
                 <h1>Department of State</h1>
+                <img className='image' src= {props.person.image}></img>
                 <h5>{props.person.name}</h5>
                 <h5>{props.person.dl}</h5>
                 <h5>{props.person.dob}</h5>
                 <h5>{props.person.passportnumber}</h5>
                 <h5>{props.person.passportexpiration}</h5>
-                <img className='image' src= {props.person.image}></img>
             </Col>
 
         </Row>
