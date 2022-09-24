aee3e4d6ba4e1e73e1fa5ff52437ddf1850641dc
timothydn2003 <87452000+timothydn2003@users.noreply.github.com>
2022-09-24 16:00:33 -0700

    nav bar link

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -3,20 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../App.css'
+import {Link} from 'react-router-dom'
 
 function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className='nav-bar'>
       <Container>
-        <Navbar.Brand href="#home"><h3>TravelX</h3></Navbar.Brand>
+        <Navbar.Brand><Link to={'/'}><h3>TravelX</h3></Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
             </Nav>
             <Nav>
-                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link><Link to = {'/'}>Home</Link></Nav.Link>
                 <NavDropdown align={"end"} title="Edit" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="/addPerson">Add a New Person</NavDropdown.Item>
+                <NavDropdown.Item ><Link to={'/addPerson'}>Add a New Person</Link></NavDropdown.Item>
                 </NavDropdown>
             </Nav>
         </Navbar.Collapse>
